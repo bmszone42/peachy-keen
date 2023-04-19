ce8dba5c88a446531ed74e68f6e43b0739c4261c
bmszone42 <122175396+bmszone42@users.noreply.github.com>
2023-04-18 20:14:05 -0400

    Initial app code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,44 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+import os
 import streamlit as st
+from llama_index import GPTSimpleVectorIndex, SimpleDirectoryReader, ServiceContext
+from llama_index.llm_predictor.chatgpt import ChatGPTLLMPredictor
 
-"""
-# Welcome to Streamlit!
+index_name = "./index.json"
+documents_folder = "./documents"
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+@st.cache_resource
+def initialize_index(index_name, documents_folder):
+    llm_predictor = ChatGPTLLMPredictor()
+    service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor)
+    if os.path.exists(index_name):
+        index = GPTSimpleVectorIndex.load_from_disk(index_name, service_context=service_context)
+    else:
+        documents = SimpleDirectoryReader(documents_folder).load_data()
+        index = GPTSimpleVectorIndex.from_documents(documents, service_context=service_context)
+        index.save_to_disk(index_name)
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+    return index
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+@st.cache_data(max_entries=200, persist=True)
+def query_index(_index, query_text):
+    response = _index.query(query_text)
+    return str(response)
 
+st.title("🦙 Llama Index Demo 🦙")
+st.header("Welcome to the Llama Index Streamlit Demo")
+st.write("Enter a query about Paul Graham's essays. You can check out the original essay [here](https://raw.githubusercontent.com/jerryjliu/llama_index/main/examples/paul_graham_essay/data/paul_graham_essay.txt). Your query will be answered using the essay as context, using embeddings from text-ada-002 and LLM completions from ChatGPT. You can read more about Llama Index and how this works in [our docs!](https://gpt-index.readthedocs.io/en/latest/index.html)")
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+index = initialize_index(index_name, documents_folder)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+text = st.text_input("Query text:", value="What did the author do growing up?")
 
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+if st.button("Run Query") and text is not None:
+    response = query_index(index, text)
+    st.markdown(response)
+    
+    llm_col, embed_col = st.columns(2)
+    with llm_col:
+        st.markdown(f"LLM Tokens Used: {index.service_context.llm_predictor._last_token_usage}")
+    
+    with embed_col:
+        st.markdown(f"Embedding Tokens Used: {index.service_context.embed_model._last_token_usage}")
