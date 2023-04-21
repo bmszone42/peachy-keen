a96205aa6bec7e86a793e4cabe5bc86008a65313
bmszone42 <122175396+bmszone42@users.noreply.github.com>
2023-04-21 13:42:49 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -171,7 +171,9 @@ if datafile is not None:
 
     #st.session_state.file_names.extend([os.path.basename(doc.source_path) for doc in new_documents])
     #st.session_state.file_names.extend([os.path.basename(str(doc.id)) for doc in new_documents])
-    st.session_state.file_names.extend([os.path.basename(doc.metadata["file_name"]) for doc in new_documents])
+    #st.session_state.file_names.extend([os.path.basename(doc.metadata["file_name"]) for doc in new_documents])
+    st.session_state.file_names.extend([os.path.basename(doc.source_path) for doc in new_documents])
+
 
 
 
