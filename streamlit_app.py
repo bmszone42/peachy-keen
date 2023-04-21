e07c46af2f5fbf808a011b03611fea2fef0bd60a
bmszone42 <122175396+bmszone42@users.noreply.github.com>
2023-04-20 20:42:53 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,7 +64,7 @@ def select_avatar_seed():
 
     st.sidebar.subheader("Avatar Settings")
     col1, col2 = st.sidebar.columns([2, 1])
-    user_avatar_seed = col1.slider("Choose your avatar seed", min_value=100, max_value=999, value=st.session_state.user_avatar_seed, help('Choose your Avatar'))
+    user_avatar_seed = col1.slider("Choose your avatar seed", min_value=100, max_value=999, value=st.session_state.user_avatar_seed)
 
     st.session_state.user_avatar_seed = user_avatar_seed
 
