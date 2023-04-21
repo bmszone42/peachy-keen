010131d92c3b2145a591920a75622723e3a7a349
bmszone42 <122175396+bmszone42@users.noreply.github.com>
2023-04-21 12:21:45 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -158,25 +158,16 @@ if datafile is not None:
         st.sidebar.success('Reindexed files successfully.')
         
     elif index_option == "Add New Files":
+        if 'index' not in st.session_state:
+            if os.path.exists('index.json'):
+                st.session_state.index = GPTSimpleVectorIndex.load_from_disk('index.json')
+            else:
+                st.session_state.index = GPTSimpleVectorIndex.from_documents([])
         new_documents = SimpleDirectoryReader('data').load_data()
-        if 'index' not in st.session_state or st.session_state.index is None:
-            st.session_state.index = GPTSimpleVectorIndex.from_documents(new_documents)
-        else:
-            merged_documents = merge_documents(st.session_state.index._documents, new_documents)
-            st.session_state.index = GPTSimpleVectorIndex.from_documents(merged_documents)
+        merged_documents = merge_documents(st.session_state.index, GPTSimpleVectorIndex.from_documents(new_documents))
+        st.session_state.index = GPTSimpleVectorIndex.from_documents(merged_documents)
         st.session_state.index.save_to_disk('index.json')
-
-#     elif index_option == "Add New Files":
-#         if 'index' not in st.session_state:
-#             if os.path.exists('index.json'):
-#                 st.session_state.index = GPTSimpleVectorIndex.load_from_disk('index.json')
-#             else:
-#                 st.session_state.index = GPTSimpleVectorIndex.from_documents([])
-#         new_documents = SimpleDirectoryReader('data').load_data()
-#         merged_documents = merge_documents(st.session_state.index, GPTSimpleVectorIndex.from_documents(new_documents))
-#         st.session_state.index = GPTSimpleVectorIndex.from_documents(merged_documents)
-#         st.session_state.index.save_to_disk('index.json')
-#         st.sidebar.success('New file added to index successfully.')
+        st.sidebar.success('New file added to index successfully.')
 
     # Add a file preview
     st.markdown("**File Preview:**")
