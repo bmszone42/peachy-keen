e6016138f1f0538ee90176e9d68d6197866716c3
bmszone42 <122175396+bmszone42@users.noreply.github.com>
2023-04-21 09:11:00 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,14 +79,15 @@ def get_bot_response(user_query, index):
 #     display_avatar_in_sidebar("adventurer", st.session_state.user_avatar_seed)
 
 # Show the avatar selected in the sidebar
-@st.cache_data
+@st.cache_data(allow_output_mutation=True, suppress_st_warning=True, show_spinner=False, max_entries=100, ttl=None, hash_funcs=None, dependencies=("avatar_style", "seed"))
 def display_avatar_in_sidebar(avatar_style, seed):
     avatar_url = f'https://avatars.dicebear.com/api/{avatar_style}/{seed}.svg'
     st.sidebar.image(avatar_url, caption=f'Your Avatar is  seed: {seed}', use_column_width=True)
 
 def update_avatar():
-    st.session_state.user_avatar_seed = st.session_state.avatar_seed
-    display_avatar_in_sidebar("adventurer", st.session_state.user_avatar_seed)
+    if st.session_state.avatar_seed != st.session_state.user_avatar_seed:
+        st.session_state.user_avatar_seed = st.session_state.avatar_seed
+        display_avatar_in_sidebar("adventurer", st.session_state.user_avatar_seed)
 
 def select_avatar_seed():
     if "user_avatar_seed" not in st.session_state:
