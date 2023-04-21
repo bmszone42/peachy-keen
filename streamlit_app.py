714cac252e53194cd2c104ae33747941cb69b26a
bmszone42 <122175396+bmszone42@users.noreply.github.com>
2023-04-20 21:46:41 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,9 @@ def get_bot_response(user_query, index):
 # Show the avatar selected in the sidebar
 def display_avatar_in_sidebar(avatar_style, seed):
     avatar_url = f'https://avatars.dicebear.com/api/{avatar_style}/{seed}.svg'
-    st.sidebar.image(avatar_url, caption=f'Your Avatar is  seed: {seed}', use_column_width=True)
+    #st.sidebar.image(avatar_url, caption=f'Your Avatar is  seed: {seed}', use_column_width=True)
+    st.sidebar.slider("Choose your avatar seed", min_value=100, max_value=999, value=st.session_state.avatar_seed, on_change=update_avatar)
+
 
 def update_avatar():
     st.session_state.user_avatar_seed = st.session_state.avatar_seed
