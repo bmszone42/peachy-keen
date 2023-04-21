3c155ff7abadc6a8da8c39b81260810dfb160768
bmszone42 <122175396+bmszone42@users.noreply.github.com>
2023-04-20 20:08:48 -0400

    Fix column warning

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,7 +61,7 @@ def select_avatar_seed():
         st.session_state.user_avatar_seed = random.randint(100, 999)
 
     st.sidebar.subheader("Avatar Settings")
-    col1, col2 = st.sidebar.beta_columns([1, 1])
+    col1, col2 = st.sidebar.columns([1, 1])
     user_avatar_seed = col1.slider("Choose your avatar seed", min_value=100, max_value=999, value=st.session_state.user_avatar_seed, label_visibility = 'visible')
 
     st.session_state.user_avatar_seed = user_avatar_seed
